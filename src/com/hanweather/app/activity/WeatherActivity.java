7299c135c7cfa4616f09d2e8a1a519ac971218bc
unknown <670654360@qq.com>
2015-12-22 16:07:40 +0800

    Last

--- src/com/hanweather/app/activity/WeatherActivity.java
+++ src/com/hanweather/app/activity/WeatherActivity.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.hanweather.app.service.AutoUpdateService;
 import com.hanweather.app.util.HttpCallbackListener;
 import com.hanweather.app.util.HttpUtil;
 import com.hanweather.app.util.Utility;
@@ -144,5 +145,7 @@ public class WeatherActivity extends Activity implements OnClickListener{
 		currentDateText.setText(prefs.getString("current_date",""));
 		weatherInfoLayout.setVisibility(View.VISIBLE);
 		cityNameText.setVisibility(View.VISIBLE);
+		Intent intent=new Intent(this,AutoUpdateService.class);
+		startService(intent);
 	}
 }
---
+++ src/com/hanweather/app/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,20 @@
+package com.hanweather.app.receiver;
+
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.hanweather.app.service.AutoUpdateService;
+
+public class AutoUpdateReceiver extends BroadcastReceiver{
+	
+
+	@Override
+	public void onReceive(Context context, Intent intent) {
+		Intent i=new Intent(context,AutoUpdateService.class);
+		context.startService(i);
+		
+	}
+
+}
